597a93626f80c564cfde5f9f240b087a895337e0
Danielateresa <dany.t_87@hotmail.it>
2022-10-14 12:03:45 +0200

    What i think to do

--- Palindroma/assets/js/main.js
+++ Palindroma/assets/js/main.js
@@ -1,1 +1,10 @@
 /* Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
+
+//guardo la documentazione
+//chiedo la parola all'utente con prompt inserendola in una costante
+//forse dovrei usare .split per separare la parola che ho schiesto all'utente
+//dopo dovrei creare un array delle singole lettere della parola (8?)
+//con .reverse dovrei poter invertire l'ordine degli elementi dell'array
+//In qualche modo dovrei rimettere insiee gli elementi (in una costante?) per formare una stringa
+//paragonare la nuova costante con la parola immessa dall'utente
+//inserire if/else in una funzione

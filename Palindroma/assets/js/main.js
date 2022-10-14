3ea3ebd39888ce223eb1b1a49b31332454ac635d
Danielateresa <dany.t_87@hotmail.it>
2022-10-14 12:15:14 +0200

    Splitted word

--- Palindroma/assets/js/main.js
+++ Palindroma/assets/js/main.js
@@ -3,8 +3,14 @@
 //guardo la documentazione
 //chiedo la parola all'utente con prompt inserendola in una costante
 //forse dovrei usare .split per separare la parola che ho schiesto all'utente
-//dopo dovrei creare un array delle singole lettere della parola (8?)
+//dopo dovrei creare un array delle singole lettere della parola (?)
 //con .reverse dovrei poter invertire l'ordine degli elementi dell'array
 //In qualche modo dovrei rimettere insiee gli elementi (in una costante?) per formare una stringa
 //paragonare la nuova costante con la parola immessa dall'utente
 //inserire if/else in una funzione
+
+
+let userWord = prompt('inserisci una parola');
+
+splittedWord = userWord.split('');
+console.log(splittedWord);
